ab8b97f55dd698f7de929aef410225854abdb4c8
Stavan <stavanj333@gmail.com>
2025-01-03 15:23:55 +0530

    working on page speed

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -13,52 +13,32 @@ export default function Hero({ data }: Herotypes) {
     return (
         <section className="bg-slate-200 px-5 sm:px-[10%] my-10 h-full">
             <div className="flex flex-col sm:flex-row items-center justify-between w-full">
-                
                 <div className="flex-1 text-center sm:text-left">
-                    
-                    <div 
-                        className="text-blue-900 scale-90 sm:scale-125 text-base"
-                        style={{ minHeight: "150px" }} 
-                    >
+                    <div className="text-blue-900 scale-90 sm:scale-125 text-base">
                         <PortableText value={data.content} components={customComponents} />
                     </div>
-
-                    
-                    <div
-                        className="flex flex-col sm:flex-row items-center sm:space-x-4 w-full sm:w-fit scale-100 sm:scale-125 mb-36 mt-12"
-                        style={{ minHeight: "60px" }} 
-                    >
+                    <div className="flex flex-col sm:flex-row items-center sm:space-x-4 w-full sm:w-fit scale-100 sm:scale-125 mb-36 mt-12">
                         {data.button.map((btnText, idx) => (
-                            <button
-                                key={idx}
-                                className="bg-gray-800 text-white px-6 py-2 mt-8 sm:mt-0 rounded-full hover:bg-red-700"
-                            >
+                            <button key={idx} className="bg-gray-800 text-white px-6 py-2 mt-8 sm:mt-0 rounded-full hover:bg-red-700">
                                 {btnText}
                             </button>
                         ))}
                     </div>
                 </div>
-
-
                 <div className="flex-1 mt-14 sm:mt-0">
                     <Image
                         src={urlFor(data.homeImage).url()}
                         alt={data.title}
                         width={560}
                         height={560}
-                        layout="intrinsic" 
                         className="hidden sm:block ml-28 sm:ml-40 sm:w-2/3 max-w-sm sm:max-w-full"
                     />
                 </div>
             </div>
+            <p className="text-blue-900 text-justify mt-10 sm:mt-20 text-sm sm:text-2xl">
+  {data.info}
+</p>
 
-            
-            <p
-                className="text-blue-900 text-justify mt-10 sm:mt-20 text-sm sm:text-2xl"
-                style={{ minHeight: "100px" }} 
-            >
-                {data.info}
-            </p>
         </section>
     );
 }
