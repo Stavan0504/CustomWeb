d677e5cfe66bb233f17bceab945e303df8528dfb
Stavan <stavanj333@gmail.com>
2025-01-03 15:29:40 +0530

    working on page speed

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -30,6 +30,7 @@ export default function Hero({ data }: Herotypes) {
                         src={urlFor(data.homeImage).url()}
                         alt={data.title}
                         width={560}
+                        loading='eager'
                         height={560}
                         className="hidden sm:block ml-28 sm:ml-40 sm:w-2/3 max-w-sm sm:max-w-full"
                     />
