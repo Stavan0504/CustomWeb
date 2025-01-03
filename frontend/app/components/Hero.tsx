2cd8b938e751c452dc3916f1f9c50e43da986d8b
Stavan <stavanj333@gmail.com>
2025-01-03 16:46:34 +0530

    working on page speed

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -29,8 +29,8 @@ export default function Hero({ data }: Herotypes) {
                     <Image
                         src={urlFor(data.homeImage).url()}
                         alt={data.title}
-                        width={638}
-                        height={640}
+                        width={538}
+                        height={540}
                         className="hidden sm:block ml-28 sm:ml-40 sm:w-2/3 max-w-sm sm:max-w-full"
                     />
                 </div>
