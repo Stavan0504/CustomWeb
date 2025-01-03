adca9958ddceda4b343282d69e369cea90b85020
Stavan <stavanj333@gmail.com>
2025-01-03 15:12:06 +0530

    working on page speed

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -25,18 +25,21 @@ export default function Hero({ data }: Herotypes) {
                     </div>
                 </div>
                 <div className="flex-1 mt-14 sm:mt-0">
+
                     <Image
+                        alt="Hero Section"
                         src={urlFor(data.homeImage).url()}
-                        alt={data.title}
                         width={560}
                         height={560}
-                        className="hidden sm:block ml-28 sm:ml-40 sm:w-2/3 max-w-sm sm:max-w-full"
+                        layout="responsive"
+                        priority={false}
                     />
+
                 </div>
             </div>
             <p className="text-blue-900 text-justify mt-10 sm:mt-20 text-sm sm:text-2xl">
-  {data.info}
-</p>
+                {data.info}
+            </p>
 
         </section>
     );
