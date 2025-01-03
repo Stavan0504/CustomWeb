25a6ee2f216e4ed13f3c213a83208c93326f6fdc
Stavan <stavanj333@gmail.com>
2025-01-03 12:06:10 +0530

    page speed

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -26,7 +26,7 @@ export default function Hero({ data }: Herotypes) {
                     </div>
                 </div>
                 <div className="flex-1 mt-14 sm:mt-0">
-                    <Image src={urlFor(data.homeImage).url()} alt={data.title} width={600} height={600} className="w-7/12 ml-16 sm:ml-40 sm:w-2/3 max-w-sm sm:max-w-full" />
+                    <Image src={urlFor(data.homeImage).url()} alt={data.title} width={600} height={600} className="w-1/2 ml-24 sm:ml-40 sm:w-2/3 max-w-sm sm:max-w-full" />
                 </div>
             </div>
             <h1 className="text-blue-900 text-justify w-full mt-10 sm:mt-20 text-xl sm:text-2xl">{data.info}</h1>
